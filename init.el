ffaa375513a858b2a3f144a2f01be16d54414eb1
Sergio García Mondaray <sgmonda@gmail.com>
2018-10-11 09:56:01 +0200

    [feature] jsx and coffee

--- init.el
+++ init.el
@@ -68,7 +68,8 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 ;;(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-;;(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.cjsx\\'" . coffee-mode))
 
 ;;; Mac OS X
 (when (eq system-type 'darwin)
